785f8a565c2fdc0c73d03246e00f913b37e70aef
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2025-12-16 17:40:08 +0100

    chore(database) : update databases col names to avoid doubled names

--- Database/assad.sql
+++ Database/assad.sql
@@ -3,57 +3,59 @@ CREATE DATABASE assad;
 USE assad;
 
 CREATE TABLE utilisateur(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_user INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(50),
     email VARCHAR(50),
-    `role` VARCHAR(50),
-    isactive BOOLEAN DEFAULT 1, 
+    `role` VARCHAR(50) DEFAULT 'visiteur',
+    isactive BOOLEAN DEFAULT 1,
     motpasse_hash VARCHAR(100)
 );
 
 CREATE TABLE visitesguidees(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_visite INT AUTO_INCREMENT PRIMARY KEY,
     titre VARCHAR(50),
     dateheure DATETIME DEFAULT CURRENT_TIMESTAMP,
     langue VARCHAR(50),
     capacite_max INT,
-    `status` ENUM('admin', 'guide', 'visitor'),
+    `status` VARCHAR(50) DEFAULT 'OFFLINE',
     duree INT,
-    prix DECIMAL(10,2)
+    prix DECIMAL(10,2),
+    id_user INT,
+    FOREIGN KEY (id_user) REFERENCES utilisateur(id_user)
 );
 
 CREATE TABLE etapesvisite(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_etape INT AUTO_INCREMENT PRIMARY KEY,
     titreetape VARCHAR(50),
     descriptionetape TEXT,
     ordreetape INT,
-    idvisite INT,
-    FOREIGN KEY (idvisite) REFERENCES visitesguidees(id) ON DELETE CASCADE
+    id_visite INT,
+    FOREIGN KEY (id_visite) REFERENCES visitesguidees(id_visite) ON DELETE CASCADE
 );
 
 CREATE TABLE commentaire(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    idvisiteguides INT,
-    idutilisateur INT,
+    id_comment INT AUTO_INCREMENT PRIMARY KEY,
+    id_visite INT,
+    id_user INT,
     note INT,
     texte TEXT,
     date_commentaire DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (idvisiteguides) REFERENCES visitesguidees(id) ON DELETE CASCADE,
-    FOREIGN KEY (idutilisateur) REFERENCES utilisateur(id) ON DELETE CASCADE
+    FOREIGN KEY (id_visite) REFERENCES visitesguidees(id_visite) ON DELETE CASCADE,
+    FOREIGN KEY (id_user) REFERENCES utilisateur(id_user) ON DELETE CASCADE
 );
 
 CREATE TABLE reservation(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    idvisite INT,
-    idutilisateur INT, 
+    id_reservation INT AUTO_INCREMENT PRIMARY KEY,
+    id_visite INT,
+    id_user INT, 
     nbpersonnes INT,
     datereservations DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (idutilisateur) REFERENCES utilisateur(id) ON DELETE CASCADE,
-    FOREIGN KEY (idvisite) REFERENCES visitesguidees(id) ON DELETE CASCADE
+    FOREIGN KEY (id_user) REFERENCES utilisateur(id_user) ON DELETE CASCADE,
+    FOREIGN KEY (id_visite) REFERENCES visitesguidees(id_visite) ON DELETE CASCADE
 );
 
 CREATE TABLE habitat(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_habitat INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(50),
     typeclimat VARCHAR(50),
     `description` TEXT,
@@ -61,7 +63,7 @@ CREATE TABLE habitat(
 );
 
 CREATE TABLE animal(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_animal INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(50),
     espece VARCHAR(50),
     alimentation VARCHAR(50),
@@ -69,5 +71,5 @@ CREATE TABLE animal(
     paysorigine VARCHAR(50),
     `description` TEXT,
     id_habitat INT,
-    FOREIGN KEY (id_habitat) REFERENCES habitat(id) ON DELETE CASCADE
+    FOREIGN KEY (id_habitat) REFERENCES habitat(id_habitat) ON DELETE CASCADE
 ); 
