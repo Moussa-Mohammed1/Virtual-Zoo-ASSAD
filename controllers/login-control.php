1bf4df7bc90d1bf2e67c690c63ade983ef3a1b8f
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2025-12-19 10:43:13 +0100

    feat: login controller to handle user login

--- controllers/login-control.php
+++ controllers/login-control.php
@@ -1,42 +1,50 @@
 <?php
-    include "./../config.php";
-    session_start();
-    $name = trim($_POST['nom']) ?? '';
-    $password = $_POST['password'] ?? '';
-    $loggeduser = null;
-    $statement = $conn->prepare("SELECT * FROM utilisateur WHERE nom = ?");
-    $statement->bind_param('s', $name);
-    $statement->execute();
-    $result = $statement->get_result();
-    echo $name ."  " . $password ;
-    if($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            if (password_verify($password, $row['motpasse_hash'])) {
-                $loggeduser = $row;
-                break;
-            }
-        }
-        if ($result->num_rows === 1 && !isset($loggeduser)) {
-            $_SESSION['incorrectpassword'] = "Incorrect password";
-            header("Location: ./../auth/login.php");
-            exit();
+include "./../config.php";
+
+session_start();
+$name = trim($_POST['nom']) ?? '';
+$password = $_POST['password'] ?? '';
+$loggeduser = null;
+$statement = $conn->prepare("SELECT * FROM utilisateur WHERE nom = ?");
+$statement->bind_param('s', $name);
+$statement->execute();
+$result = $statement->get_result();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        if (password_verify($password, $row['motpasse_hash'])) {
+            $loggeduser = $row;
+            break;
         }
     }
-    else{
-        $_SESSION['nouser'] = "Incorrect Username or password";
+    if ($result->num_rows === 1 && !isset($loggeduser)) {
+        $_SESSION['incorrectpassword'] = "Incorrect password";
+        header("Location: ./../auth/login.php");
+        exit();
     }
-    if ($loggeduser) {
-        $_SESSION['loggeduser'] = $loggeduser;
-        if (!$loggeduser['approved']) {
-            header('Location: ./../auth/approval.php');
-            exit();
-        }
-        if ($loggeduser['role'] == 'visiteur') {
+} else {
+    $_SESSION['nouser'] = "Incorrect Username or password";
+    header("Location: ./../auth/login.php");
+    exit();
+}
+if ($loggeduser) {
+    $_SESSION['loggeduser'] = $loggeduser;
+
+    if (!$loggeduser['approved']) {
+        header('Location: ./../auth/approval.php');
+        exit();
+    }
+
+    switch ($loggeduser['role']) {
+        case 'admin':
+            header('Location: ./../Admin/dashboard.php');
+            break;
+        case 'visiteur':
             header('Location: ./../Visitor/visitor.php');
-            exit();
-        }
-        if ($loggeduser['role'] == 'guide') {
+            break;
+        case 'guide':
             header('Location: ./../Guide/guide.php');
-            exit();
-        }
+            break;
     }
+    exit();
+}
